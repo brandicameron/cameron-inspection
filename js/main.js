802735229db9f2452670b81bf53298f5cd26feb3
Brandi <47793095+brandicameron@users.noreply.github.com>
2024-10-19 14:26:46 -0500

    added closed notice

--- js/main.js
+++ js/main.js
@@ -3,4 +3,6 @@ document.getElementById('copyYear').textContent = currentYear.$y;
 const startYear = dayjs('2002-01-01');
 
 const yearsOfExperience = currentYear.diff(startYear, 'year');
-document.getElementById('years-experience').textContent = yearsOfExperience;
+
+if (document.getElementById('years-experience'))
+  document.getElementById('years-experience').textContent = yearsOfExperience;
